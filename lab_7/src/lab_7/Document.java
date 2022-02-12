d9fdab55e6cae79b16ab6cb847d2983624e6e1ea
joydeep-m <jmukherj@calpoly.edu>
2022-02-11 16:46:25 -0800

    First Commit

---
+++ lab_7/src/lab_7/Document.java
@@ -0,0 +1,24 @@
+package lab_7;
+
+public class Document {
+
+	/**
+	 * document filename from which to read from and write into
+	 */
+	public static final String filename="text.txt";
+	
+	/**
+	 * reads content from the file and displays to the terminal
+	 */
+	void readFile() {
+		
+    }
+	
+	/**
+	 * writes the String "CSE 305, Cal Poly" into the file
+	 */
+	void writeFile() {
+		
+	}
+	
+}
